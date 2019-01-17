6100f91bd358c012849b1ef51c4ee1216449654f
Yago Vila <detiraosvila@gmail.com>
2019-01-17 10:46:08 +0100

    Database connection refactored

---
+++ database.php
@@ -0,0 +1,17 @@
+
+<?php
+	// Credenciales base de datos
+	 $servername = "localhost";
+	 $username = "alan";
+	 $password = "turing";		
+	 $database = "ENIGMA";
+
+	//Crear conection MySQL
+ 	$conn =mysqli_connect($servername, $username, $password, $database);	
+	
+	//Comprobar conexion, se falla mostrar erro
+ 	if (!$conn) {
+ 	die('<p>Fallou a conexión coa base de datos: </p>'. mysqli_connect_error());
+ 	}
+ 	echo '<p>Conexión OK!</p>';
+?>
